d490667255051be6c5dd73f12b449e330dc7f9b1
Rosa Fox <rosafox89@gmail.com>
2023-11-15 11:59:04 +0000

    Display answers for specific feedback value
    - Displays prompts and answers for feedback response 'responses_were_useful'.
    - This is the first step in being able to click on the responses segments of the
    feedback charts to view the related Answers.
    - In the next commit we will change this to pass feedback values from clicking
    a chart.

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,9 @@
 class AnswersController < ApplicationController
+
+  def index
+    @answers = Answer.where(header: "responses_were_useful", value: "Strongly agree")
+  end
+
   def show
     @answer = Answer.find(params[:id])
   end
