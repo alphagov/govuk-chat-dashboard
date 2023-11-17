3fd8da02d0037be975aa341a31d1a645618c6790
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2023-11-17 10:57:54 +0000

    Make importer class idempotent
    As we are exporting the CSV data from the chat application in it's entirety and
    not using snapshots, and as we want the dashboard to be in-sync with the chat
    application on completion of the export/import process. We want the importer that
    imports the exported CSV data to be idempotent.
    
    That is, no matter how many times we run the importer - we want the exact result each
    time. In this case, each time we should end up with the same number of records
    that have been exported to be imported for both the Chat and Feedback models, have
    the correct number of matching Answer records created, and have the same primary and
    foreign keys created so that the model realtionships remain intact.
    
    Only when the latest CSV export includes additional data, should the numbers of
    records change and increase accordingly.
    
    For our purposes we can do this simply by deleting the existing records - that we
    no longer need as they will be replicated, then resetting the primary key sequence
    on those specific tables.

--- app/models/importer.rb
+++ app/models/importer.rb
@@ -3,39 +3,51 @@ require "google/cloud/storage"
 
 class Importer
   def self.import_chat
-    csv_data = read_file("chat")
-    csv_data.each do |row|
-      Chat.create!(
-        id: row["id"],
-        uuid: row["uuid"],
-        prompt: row["prompt"],
-        answer: row["answer"],
-        sources: row["sources"],
-        created_at: row["created_at"],
-        updated_at: row["updated_at"],
-      )
+    ActiveRecord::Base.transaction do
+      Chat.delete_all
+      ActiveRecord::Base.connection.reset_pk_sequence!("chats")
+
+      csv_data = read_file("chat")
+      csv_data.each do |row|
+        Chat.create!(
+          id: row["id"],
+          uuid: row["uuid"],
+          prompt: row["prompt"],
+          answer: row["answer"],
+          sources: row["sources"],
+          created_at: row["created_at"],
+          updated_at: row["updated_at"],
+        )
+      end
     end
   end
 
   def self.import_feedback
-    csv_data = read_file("feedback")
-    csv_data.each do |row|
-      feedback = Feedback.create!(
-        id: row["id"],
-        chat_id: row["chat_id"],
-        uuid: row["uuid"],
-        version: row["version"],
-        level: row["level"],
-        created_at: row["created_at"],
-        updated_at: row["updated_at"],
-      )
+    ActiveRecord::Base.transaction do
+      Answer.delete_all
+      Feedback.delete_all
+      ActiveRecord::Base.connection.reset_pk_sequence!("answers")
+      ActiveRecord::Base.connection.reset_pk_sequence!("feedbacks")
 
-      ["was_answer_useful", "responses_were_useful", "experience_with_chat", "trusted_the_answers", "waiting_response_time", "inaccurate_or_inconsistent", "did_you_know_answers", "best_describes_you", "how_old_are_you", "internet_skill_rating", "any_other_comments"].each do |header|
-        Answer.create!(
-          feedback_id: feedback.id,
-          header: header,
-          value: row[header],
+      csv_data = read_file("feedback")
+      csv_data.each do |row|
+        feedback = Feedback.create!(
+          id: row["id"],
+          chat_id: row["chat_id"],
+          uuid: row["uuid"],
+          version: row["version"],
+          level: row["level"],
+          created_at: row["created_at"],
+          updated_at: row["updated_at"],
         )
+
+        ["was_answer_useful", "responses_were_useful", "experience_with_chat", "trusted_the_answers", "waiting_response_time", "inaccurate_or_inconsistent", "did_you_know_answers", "best_describes_you", "how_old_are_you", "internet_skill_rating", "any_other_comments"].each do |header|
+          Answer.create!(
+            feedback_id: feedback.id,
+            header: header,
+            value: row[header],
+          )
+        end
       end
     end
   end
