ed0033bd7900c385d2f1f97b85c956aeaf83f8c9
Pete Goddard <pete@artofsoftware.co.uk>
2023-12-06 17:21:35 +0000

    Remove routes we don't use

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   get 'written/index'
   get 'explore/index'
   devise_for :users
-  resources :tags
-  resources :feedbacks
-  resources :chats
-  resources :answers
+  resources :tags, except: [:new, :edit], defaults: { format: :json }
+  resources :feedbacks, only: [:create]
+  resources :chats, only: [:create]
+  resources :answers, only: [:index, :show]
   root to: "home#index"
 end
