8487285e102786043a71160529a8aebeaa7db15b
Pete Goddard <pete@artofsoftware.co.uk>
2023-12-08 10:07:55 +0000

    Add test for /explore/index

--- test/controllers/explore_controller_test.rb
+++ test/controllers/explore_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class ExploreControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test "get index contains a script block" do
+    sign_in(users(:user))
     get explore_index_url
     assert_response :success
+    assert_includes response.body, 'script'
+  end
+
+  test "get index redirects when unauthenticated" do
+    get explore_index_url
+    assert_redirected_to new_user_session_path
   end
 end
