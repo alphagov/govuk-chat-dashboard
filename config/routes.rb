109f50de3ae8615b6d6b11f0d556af9574eaf198
Pete Goddard <pete@artofsoftware.co.uk>
2023-12-11 16:33:00 +0000

    Delete /tags route as it's not needed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'written/index'
   get 'explore/index'
   devise_for :users
-  resources :tags, except: [:new, :edit], defaults: { format: :json }
   resources :answers, only: [:index, :show]
   root to: "home#index"
 end
