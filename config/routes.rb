7c204dbf9fe80b4a8b4625a55bf8b658f8ac1073
Pete Goddard <pete@artofsoftware.co.uk>
2023-12-11 12:21:27 +0000

    Delete POST /feedbacks route as it's not used

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'explore/index'
   devise_for :users
   resources :tags, except: [:new, :edit], defaults: { format: :json }
-  resources :feedbacks, only: [:create]
   resources :chats, only: [:create]
   resources :answers, only: [:index, :show]
   root to: "home#index"
