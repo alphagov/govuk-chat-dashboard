93fff1805392a8456f81c3867e32372b33d3920f
Pete Goddard <pete@artofsoftware.co.uk>
2023-12-13 16:06:00 +0000

    Add test and some validation to answers

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class AnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @answer = answers(:one)
+  end
+
+  %w[
+    was_answer_useful
+    responses_were_useful
+    experience_with_chat
+    trusted_the_answers
+    waiting_response_time
+    inaccurate_or_inconsistent
+    did_you_know_answers
+    best_describes_you
+    how_old_are_you
+    internet_skill_rating
+    any_other_comments
+  ].each do |header|
+    test "allows #{header} header" do
+      @answer.header = header
+      assert @answer.valid?
+    end
+  end
+
+  test "disallows other values for heaader" do
+    @answer.header = "something else"
+    assert_not @answer.valid?
+  end
+
+  %w[
+    header
+    value
+    feedback
+  ].each do |field|
+    test "requires a value for #{field}" do
+      @answer.send("#{field}=", nil)
+      assert_not @answer.valid?
+    end
+  end
 end
