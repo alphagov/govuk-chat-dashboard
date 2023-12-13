0ae7ad96ae5952745877f9fe7000b2655781bb7a
Pete Goddard <pete@artofsoftware.co.uk>
2023-12-13 15:26:23 +0000

    Add a test for User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:user)
+  end
+
+  test "accepts viewer, tagger as role" do
+    [:viewer, :tagger].each do |role|
+      @user.role = :viewer
+      assert @user.valid?
+    end
+  end
+
+  test "doesn't accept other roles" do
+    assert_raises(ArgumentError) { @user.role = :wrong }
+  end
 end
